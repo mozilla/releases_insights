a2aacbdab98831ad1ee1428f4c8332b29f2a8aaf
Pascal Chevrel <pascal.chevrel@free.fr>
2019-07-31 03:34:31 +0200

    simpler and more efficient way to get all beta commits for a release

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,6 @@ require_once 'config.php';
 require_once 'utils.php';
 require_once 'uplift.php';
 
-
 $url = parse_url($_SERVER['REQUEST_URI']);
 
 if (isset($url['query'])) {
@@ -28,43 +27,8 @@ if (! in_array($params['version'], $supported_releases)) {
     exit("release not supported");
 }
 
-switch ($params['version']) {
-    case '60':
-        $last_beta = '16';
-        break;
-    case '61':
-        $last_beta = '14';
-        break;
-    case '62':
-        $last_beta = '20';
-        break;
-    case '63':
-        $last_beta = '14';
-        break;
-    case '64':
-        $last_beta = '14';
-        break;
-    case '65':
-        $last_beta = '12';
-        break;
-    case '66':
-        $last_beta = '14';
-        break;
-    case '67':
-        $last_beta = '19';
-        break;
-    case '68':
-        $last_beta = '14';
-        break;
-    default:
-        break;
-}
-
 $query = 'https://hg.mozilla.org/releases/mozilla-beta/json-pushes?fromchange=FIREFOX_'
     . $params['version']
-    . '_0b3_RELEASE&tochange=FIREFOX_'
+    . '_0b3_RELEASE&tochange=FIREFOX_BETA_'
     . $params['version']
-    . '_0b'
-    . $last_beta
-    . '_RELEASE&full&version=2';
-
+    . '_END&full&version=2';
--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 require_once __DIR__ . '/../app/bootstrap.php';
 
-
 $bugs = getBugsFromHgWeb($query);
 
 // redirect to Bugzilla if &redirect is set in url
