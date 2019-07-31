d3746dc01966066f51cbdf36802fef9837e60fee
Pascal Chevrel <pascal.chevrel@free.fr>
2019-07-31 04:07:16 +0200

    add RC data

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -27,8 +27,14 @@ if (! in_array($params['version'], $supported_releases)) {
     exit("release not supported");
 }
 
-$query = 'https://hg.mozilla.org/releases/mozilla-beta/json-pushes?fromchange=FIREFOX_'
+$betaquery = 'https://hg.mozilla.org/releases/mozilla-beta/json-pushes?fromchange=FIREFOX_'
     . $params['version']
     . '_0b3_RELEASE&tochange=FIREFOX_BETA_'
     . $params['version']
     . '_END&full&version=2';
+
+$rcquery = 'https://hg.mozilla.org/releases/mozilla-release/json-pushes?fromchange=FIREFOX_BETA_'
+    . $params['version']
+    . '_END&tochange=FIREFOX_'
+    . $params['version']
+    . '_0_RELEASE&full&version=2';
--- public/index.php
+++ public/index.php
@@ -1,7 +1,17 @@
 <?php
 require_once __DIR__ . '/../app/bootstrap.php';
 
-$bugs = getBugsFromHgWeb($query);
+$betabugs = getBugsFromHgWeb($betaquery);
+$rcbugs   = getBugsFromHgWeb($rcquery);
+
+$a = array_merge($betabugs['uplifts'], $rcbugs['uplifts']);
+$b = array_merge($betabugs['backouts'], $rcbugs['backouts']);
+
+$bugs = [
+    'uplifts'   => $a,
+    'backouts'  => $b,
+    'total'     => array_values(array_merge($a, $b))
+];
 
 // redirect to Bugzilla if &redirect is set in url
 if ($redirect) {
@@ -13,14 +23,14 @@ if ($json) {
     print outputJson(
         bzBugList(
             [
-                'desc' => 'Beta uplifts and backouts',
-                'version' => $params['version'],
-                'uplifts_count' => count($bugs['uplifts']),
+                'desc'           => 'Beta/RC uplifts and backouts',
+                'version'        => $params['version'],
+                'uplifts_count'  => count($bugs['uplifts']),
                 'backouts_count' => count($bugs['backouts']),
-                'total_count' => count($bugs['total']),
-                'uplifts' => $bugs['uplifts'],
-                'backouts' => $bugs['backouts'],
-                'total' => $bugs['total']
+                'total_count'    => count($bugs['total']),
+                'uplifts'        => $bugs['uplifts'],
+                'backouts'       => $bugs['backouts'],
+                'total'          => $bugs['total']
             ],
             false
         )
@@ -28,7 +38,7 @@ if ($json) {
     exit;
 }
 
-print "<h3>Beta uplifts in Firefox ${params['version']}</h3>";
+print "<h3>Beta/RC uplifts in Firefox ${params['version']}</h3>";
 print 'Uplifts: <a href="'. bzBugList($bugs['uplifts']) . '">' . count(array_unique($bugs['uplifts'])) . '</a><br>';
 print 'Backouts: <a href="'. bzBugList($bugs['backouts']) . '">' . count($bugs['backouts']) . '</a><br>';
 print 'Total: <a href="'. bzBugList($bugs['total']) . '">' . count($bugs['total']) . '</a><br>';
