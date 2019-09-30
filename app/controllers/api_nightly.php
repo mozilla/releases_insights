35b2f72de30065eb65d2480b16c8bc136b542c71
Pascal Chevrel <pascal.chevrel@free.fr>
2019-09-30 21:47:37 +0200

    cleanup

--- app/controllers/api_nightly.php
+++ app/controllers/api_nightly.php
@@ -1,27 +1,5 @@
 <?php
 namespace ReleaseInsights;
 
-$options = [
-    'http' => [
-        'method'  => 'POST',
-        'header'  => 'Content-Type: application/json',
-        'content' => '{
-      "_source": ["build.id", "source.revision"],
-      "query": {
-        "bool": {
-          "must": [
-            { "match": { "source.product": "firefox" }},
-            { "match": { "target.platform": "win64" }},
-            { "match": { "target.locale": "en-US" }},
-            { "match": { "target.channel": "nightly" }},
-            { "regexp": { "build.target": "x86_64.*" }},
-            { "regexp": { "build.id": "' . ($_GET['date'] ?? '20190101') . '.*" }}
-          ]
-        }
-      }
-    }'
-    ]
-];
-
 $json = include MODELS . 'api_nightly.php';
 require_once VIEWS . 'json.php';
--- app/models/api_nightly.php
+++ app/models/api_nightly.php
@@ -1,6 +1,28 @@
 <?php
 use Cache\Cache;
 
+$options = [
+    'http' => [
+        'method'  => 'POST',
+        'header'  => 'Content-Type: application/json',
+        'content' => '{
+      "_source": ["build.id", "source.revision"],
+      "query": {
+        "bool": {
+          "must": [
+            { "match": { "source.product": "firefox" }},
+            { "match": { "target.platform": "win64" }},
+            { "match": { "target.locale": "en-US" }},
+            { "match": { "target.channel": "nightly" }},
+            { "regexp": { "build.target": "x86_64.*" }},
+            { "regexp": { "build.id": "' . ($_GET['date'] ?? '20190101') . '.*" }}
+          ]
+        }
+      }
+    }'
+    ]
+];
+
 $cache_id = $options['http']['content'];
 
 if (! $data = Cache::getKey($cache_id)) {
