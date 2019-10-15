e0111e9fac213bbea8cf7c2c017c9427fb13f133
Pascal Chevrel <pascal.chevrel@free.fr>
2019-10-15 13:23:20 +0200

    add caching to crashes api, filter out data we don't want from socorro

--- app/models/api_nightly_crashes.php
+++ app/models/api_nightly_crashes.php
@@ -10,4 +10,33 @@ if (!Utils::isBuildID($buildid)) {
     return ['Error' => 'Invalid build ID'];
 }
 
-return json_decode(file_get_contents('https://crash-stats.mozilla.com/api/SuperSearch/?build_id=' . $buildid . '&_facets=signature'), true);
+$cache_id = 'https://crash-stats.mozilla.com/api/SuperSearch/?build_id=' . $buildid . '&_facets=signature';
+
+// If we can't retrieve cached data, we create and cache it.
+// We cache because we want to avoid http request latency
+if (!$data = Cache::getKey($cache_id)) {
+    $data = file_get_contents($cache_id);
+
+    // Extract into an array the values we want from the data source
+    $data = json_decode($data, true);
+
+    // No data returned, don't cache.
+    if (empty($data)) {
+        return [];
+    }
+
+    $total_crashes = $data['total'];
+    $signatures = $data['facets']['signature'];
+
+    $data = [
+        'total' => $total_crashes,
+        'signatures' => $signatures,
+    ];
+
+    // We don't cache today because we may miss the second nightly build
+    if (date('Ymd', $buildid) != date('Ymd')) {
+        Cache::setKey($cache_id, $data);
+    }
+}
+
+return $data;
