0526a9d1f280e2580de6f7b56756df4cdd451de1
Pascal Chevrel <pascal.chevrel@free.fr>
2019-10-24 01:18:01 +0200

    Navigation links to prev/next nightly

--- app/models/nightly.php
+++ app/models/nightly.php
@@ -2,17 +2,24 @@
 use ReleaseInsights\Bugzilla as Bz;
 use ReleaseInsights\Utils as Utils;
 
+// We need previous and next days for navigation and changelog
+// The requester date is already in the $date variable
+$today          = date('Ymd');
+$requested_date = Utils::getDate();
+$previous_date  = date('Ymd', strtotime($requested_date . ' -1 day'));
+$next_date      = date('Ymd', strtotime($requested_date . ' +1 day'));
+
 // Get nightlies for the GET Request (or today's nightly)
 $nightlies = include MODELS . 'api_nightly.php';
 
 // Strore a value for the View title
-$display_date = date('Y M d', strtotime($date));
+$display_date = date('Y M d', strtotime($requested_date));
 
 // We now fetch the previous day nightlies because we need them for changelogs
-$_GET['date'] = date('Ymd', strtotime($date . ' -1 day'));
+$_GET['date'] = $previous_date;
 $nightlies_day_before = include MODELS . 'api_nightly.php';
 
-// Associate nightly with the previous one
+// Associate nightly with nightly-1
 $nightly_pairs = [];
 
 $i = true;
@@ -40,7 +47,7 @@ $build_crashes = [];
 foreach ($nightly_pairs as $dataset) {
     $build_crashes[$dataset['buildid']] = Utils::getCrashesForBuildID($dataset['buildid'])['total'];
 }
-cli_dump($build_crashes);
+
 $top_sigs = [];
 foreach ($nightly_pairs as $dataset) {
     $top_sigs[$dataset['buildid']] = array_splice(
