024d9addc0b667a142a698a2c3397eff6eb91d23
Pascal Chevrel <pascal.chevrel@free.fr>
2020-04-24 18:10:27 +0200

    add a Utils::cli_dump() command to var_dump colored structured objects to the dev server

--- app/classes/ReleaseInsights/Utils.php
+++ app/classes/ReleaseInsights/Utils.php
@@ -178,4 +178,17 @@ class Utils
 
         return $matches == count($needles);
     }
+
+    /**
+     * Utility function to get symfony dump() function output to the CLI
+     * http://symfony.com/doc/current/components/var_dumper/
+     */
+    function cli_dump() : void
+    {
+        $cloner = new \Symfony\Component\VarDumper\Cloner\VarCloner();
+        $dumper = new \Symfony\Component\VarDumper\Dumper\CliDumper();
+        foreach (func_get_args() as $arg) {
+            $dumper->dump($cloner->cloneVar($arg));
+        }
+    }
 }
