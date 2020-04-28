22ed94729102377e948b2317b442bb742a95d0a8
Pascal Chevrel <pascal.chevrel@free.fr>
2020-04-28 13:06:16 +0200

    prepare an overview view (with the home content as placeholder)

---
+++ app/controllers/overview.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once MODELS . 'home.php';
+
+echo $twig->render(
+    'regular.html.twig',
+    [
+        'page_title'   => $page_title,
+        'css_files'    => $css_files,
+        'css_page_id'  => $css_page_id,
+        'page_content' => $content,
+    ]
+);
