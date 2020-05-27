2b6b5cbff7c2e00b6e3d0b2cd8e404e22979ac71
Pascal Chevrel <pascal.chevrel@free.fr>
2020-05-27 23:40:15 +0200

    fix missing import in home page

--- app/models/home.php
+++ app/models/home.php
@@ -1,5 +1,8 @@
 <?php
 
+use ReleaseInsights\Utils;
+
 // Get the schedule for the current beta
 $requested_version = Utils::requestedVersion(FIREFOX_NIGHTLY);
 $cycle_dates = include MODELS . 'api_release_schedule.php';
+
