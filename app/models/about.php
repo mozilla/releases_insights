4e52acf78c9a8bf7098acc2f6e9b18341367192c
Pascal Chevrel <pascal.chevrel@free.fr>
2020-05-28 08:43:29 +0200

    update about page with new views and apis

--- app/models/about.php
+++ app/models/about.php
@@ -12,12 +12,16 @@ $content = <<<"EOD"
         <th>url</th><th>Description</th>
     </tr>
     <tr>
-        <td><a href="/api/nightly/?date={$yesterday}">/api/nightly/?date={$yesterday}</a></td>
-        <td>Provides the list of nightly buildids for a day and the changeset they were built (data from buildhub)</td>
+        <td><a href="/api/nightly/?date={$yesterday}">api/nightly/?date={$yesterday}</a></td>
+        <td>Provides the list of nightly buildids for a day and the changeset they were built from (data from buildhub)</td>
     </tr>
     <tr>
-        <td><a href="/api/nightly/crashes/?buildid=20190927094817">/api/nightly/crashes/?buildid=20190927094817</a></td>
+        <td><a href="/api/nightly/crashes/?buildid=20190927094817">api/nightly/crashes/?buildid=20190927094817</a></td>
         <td>Gives the crashes for a buildid (data from socorro)</td>
+    </tr>
+    <tr>
+        <td><a href="/api/release/schedule/?version=beta">api/release/schedule/?version=beta</a></td>
+        <td>Gives the forecast release 4 week scheduled milestones for an upcoming major version. Can be a version number or one of the <code>beta</code> or <code>nightly</code> keywords</td>
     </tr>
 </table>
 
@@ -27,20 +31,28 @@ $content = <<<"EOD"
         <th>url</th><th>Description</th>
     </tr>
     <tr>
+        <td><a href="/">/</a></td>
+        <td>Homepage with a simple overview of the releases in flight</td>
+    </tr>
+    <tr>
+        <td><a href="/about">about</a></td>
+        <td>Homepage with a simple overview of the releases in flight</td>
+    </tr>
+    <tr>
         <td>
             <ul class="list-unstyled mb-0">
-                <li><a href="/nightly/">/nightly/</a> (today)</li>
-                <li><a href="/nightly/?date={$yesterday}">/nightly/?date={$yesterday}</a></li>
+                <li><a href="/nightly/">nightly/</a> (today)</li>
+                <li><a href="/nightly/?date={$yesterday}">nightly/?date={$yesterday}</a></li>
             </ul>
         </td>
         <td>Provides the list of nightly buildids for a day, their crashes, changelog, bugs fixed</td>
     </tr>
     <tr>
-        <td><a href="/beta/">/beta</a></td>
-        <td>Provides the list of beta builds for the current release, their crashes, uplifts</td>
+        <td><a href="/beta/">beta</a></td>
+        <td>WIP: Will provides the list of beta builds for the current release and uplifts per beta</td>
     </tr>
     <tr>
-        <td><a href="/release/">/release</a></td>
+        <td><a href="/release/">release</a></td>
         <td>Provides historical data for past releases and basic release date information for future releases.</td>
     </tr>
 </table>
