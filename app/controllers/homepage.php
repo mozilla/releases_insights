e4174d77ebb3c7d9fbe42a21dec78c81b4610e20
Pascal Chevrel <pascal.chevrel@free.fr>
2020-06-02 10:42:50 +0200

    Add logic to detectr on front page if it is release day, show a nice conditional warning

--- app/controllers/homepage.php
+++ app/controllers/homepage.php
@@ -5,9 +5,11 @@ require_once MODELS . 'home.php';
 print $twig->render(
     'overview.html.twig',
     [
-        'page_title'   => $page_title,
-        'css_files'    => $css_files,
-        'css_page_id'  => $controller,
-        'cycle_dates'  => $cycle_dates,
+        'page_title'       => $page_title,
+        'css_files'        => $css_files,
+        'css_page_id'      => $controller,
+        'cycle_dates'      => $cycle_dates,
+        'release_day'      => $today_is_release_day,
+        'shipping_release' => $shipping_release,
     ]
 );
--- app/models/home.php
+++ app/models/home.php
@@ -6,3 +6,19 @@ use ReleaseInsights\Utils;
 $requested_version = Utils::requestedVersion(FIREFOX_NIGHTLY);
 $cycle_dates = include MODELS . 'api_release_schedule.php';
 
+
+
+// Historical data from Product Details, cache an hour
+$shipped_releases = Utils::getJson('https://product-details.mozilla.org/1.0/firefox_history_major_releases.json', 3600);
+$upcoming_releases = include DATA .'upcoming_releases.php';
+
+$all_releases = array_merge($shipped_releases, $upcoming_releases);
+
+$today_is_release_day = false;
+
+$today = date('Y-m-d');
+
+if (in_array($today, $all_releases)) {
+    $today_is_release_day = true;
+    $shipping_release = (int) array_search($today, $all_releases);
+}
