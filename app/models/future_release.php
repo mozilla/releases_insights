12cf326836fa894e1d4beb2c189338d3fb51c9ff
Pascal Chevrel <pascal.chevrel@free.fr>
2021-01-26 11:42:52 +0100

    fix PHP notice because we pass an undefined variable to the controller for the nightly view

--- app/models/future_release.php
+++ app/models/future_release.php
@@ -32,6 +32,7 @@ $nightly_cycle_length = $previous_release->diff($nightly_start)->days / 7;
 // Get the schedule for the release requested
 $cycle_dates = include MODELS . 'api_release_schedule.php';
 
+$nightly_fixes = 0;
 /* Only for the current Beta view */
 if ((int) $requested_version === $main_beta) {
 	// Number of bugs fixed in nightly
