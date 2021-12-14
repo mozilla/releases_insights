5d0fbed1163a024ee97f16b54e2fab001dd1afc6
Pascal Chevrel <pascal.chevrel@free.fr>
2021-12-14 16:48:29 +0100

    Replace the switch() in the dispatcher file by a match() which is a simpler syntax

--- app/inc/dispatcher.php
+++ app/inc/dispatcher.php
@@ -3,37 +3,17 @@
 declare(strict_types=1);
 
 // Here we decide what page we are asking for, falls back to a 404
-switch ($url['path']) {
-    case '/':
-        $controller = 'homepage';
-        break;
-    case 'about':
-        $controller = 'about';
-        break;
-    case 'nightly':
-        $controller = 'nightly';
-        break;
-    case 'release':
-        $controller = 'release';
-        break;
-    case 'api/nightly':
-        $controller = 'api/nightly';
-        break;
-    case 'api/release/schedule':
-        $controller = 'api/release_schedule';
-        break;
-    case 'api/release/owners':
-        $controller = 'api/release_owners';
-        break;
-    case 'api/nightly/crashes':
-        $controller = 'api/nightly_crashes';
-        break;
-    case 'calendar/release/schedule':
-        $controller = 'ics_release_schedule';
-        break;
-    default:
-        $controller = '404';
-        break;
-}
+$controller = match ($url['path']) {
+    '/'                         => 'homepage',
+    'about'                     => 'about',
+    'nightly'                   => 'nightly',
+    'release'                   => 'release',
+    'api/nightly'               => 'api/nightly',
+    'api/release/schedule'      => 'api/release_schedule',
+    'api/release/owners'        => 'api/release_owners',
+    'api/nightly/crashes'       => 'api/nightly_crashes',
+    'calendar/release/schedule' => 'ics_release_schedule',
+    default => '404',
+};
 
 include CONTROLLERS . $controller . '.php';
