ba9b262a78eb07d7811d832c98260099ee85b5bf
Pascal Chevrel <pascal.chevrel@free.fr>
2022-01-02 13:31:19 +0100

    Run local HTTP tests faster, no pause in CI. Add a .env file to control that.

--- tests/Functional/init.php
+++ tests/Functional/init.php
@@ -8,6 +8,10 @@ date_default_timezone_set('America/Los_Angeles');
 
 require __DIR__ . '/../../vendor/autoload.php';
 
+// Load the .env file to get local vs CI CONTEXT environment value
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
 // Launch PHP dev server in the background
 chdir(realpath(__DIR__ . '/../../'));
 echo getcwd();
@@ -16,5 +20,5 @@ exec('php -S localhost:8083 -t public/ > /dev/null 2>&1 & echo $!', $output);
 // We will need the pid to kill it, beware, this is the pid of the bash process started with start.sh
 $pid = $output[0];
 
-// Pause to let time for the dev server to launch in the background
-sleep(3);
+// Pause to let time for the dev server to launch in the background in CI, locally it's instant
+sleep($_ENV['CONTEXT'] == 'local' ? 0 : 3);
