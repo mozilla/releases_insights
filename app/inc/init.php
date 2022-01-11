1cb9dc3e8d57cf66133e6e2dd00f729b95dda3a8
Pascal Chevrel <pascal.chevrel@free.fr>
2022-01-11 16:28:07 +0100

    Fix a caching bug for p-d

--- app/inc/init.php
+++ app/inc/init.php
@@ -18,6 +18,11 @@ date_default_timezone_set('America/Los_Angeles');
 define('INSTALL_ROOT', realpath(__DIR__ . '/../../') . '/');
 require_once INSTALL_ROOT . 'vendor/autoload.php';
 
+// Prepare caching
+define('CACHE_PATH', INSTALL_ROOT . 'cache/');
+define('CACHE_ENABLED', ! isset($_GET['nocache']));
+define('CACHE_TIME', 3600 * 72);
+
 // Cache Product Details versions, 12H cache
 $firefox_versions = Utils::getJson(
     'https://product-details.mozilla.org/1.0/firefox_versions.json',
@@ -30,15 +35,12 @@ define('FIREFOX_NIGHTLY', $firefox_versions['FIREFOX_NIGHTLY']);
 define('DEV_EDITION', $firefox_versions['FIREFOX_DEVEDITION']);
 define('FIREFOX_BETA', $firefox_versions['LATEST_FIREFOX_RELEASED_DEVEL_VERSION']);
 define('FIREFOX_RELEASE', $firefox_versions['LATEST_FIREFOX_VERSION']);
-define('CACHE_ENABLED', ! isset($_GET['nocache']));
-define('CACHE_TIME', 3600 * 72);
 
 // Application globals paths
 const DATA        = INSTALL_ROOT . 'app/data/';
 const VIEWS       = INSTALL_ROOT . 'app/views/';
 const MODELS      = INSTALL_ROOT . 'app/models/';
 const CONTROLLERS = INSTALL_ROOT . 'app/controllers/';
-const CACHE_PATH  = INSTALL_ROOT . 'cache/';
 
 $main_nightly = (int) FIREFOX_NIGHTLY;
 $main_beta    = (int) FIREFOX_BETA;
