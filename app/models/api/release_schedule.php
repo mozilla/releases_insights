9c23fb36e359250efe90b1c3dd1db51f291e772d
Pascal Chevrel <pascal.chevrel@free.fr>
2022-01-13 15:42:23 +0100

    remove 96 customization to the schedule

--- app/models/api/release_schedule.php
+++ app/models/api/release_schedule.php
@@ -38,7 +38,7 @@ $nightly->modify('-1 day');
 $date_format = 'Y-m-d H:i';
 
 $x = match ($requested_version) {
-    '96.0', '97.0' => 4,
+    '97.0' => 4,
     default => 3,
 };
 
@@ -61,31 +61,7 @@ $schedule = [
     'release'          => $release->format($date_format),
 ];
 
-if ($requested_version === '96.0') {
-    $nightly = new DateTime($all_releases[Version::decrement($requested_version, 2)]);
-    $nightly->modify('-1 day');
 
-    $schedule = [
-        'nightly_start'    => $nightly->format($date_format),
-        'soft_code_freeze' => $nightly->modify('+' . $x .' weeks')->modify('Thursday')->format($date_format),
-        'string_freeze'    => $nightly->modify('Friday')->format($date_format),
-        'merge_day'        => $nightly->modify('Monday')->format($date_format),
-        'beta_1'           => $nightly->modify('Monday')->format($date_format),
-        'beta_2'           => $nightly->modify('Tuesday')->format($date_format),
-        'beta_3'           => $nightly->modify('Thursday')->format($date_format),
-        'beta_4'           => $nightly->modify('Sunday')->format($date_format),
-        'beta_5'           => $nightly->modify('Tuesday')->format($date_format),
-        'beta_6'           => $nightly->modify('Thursday')->format($date_format),
-        'beta_7'           => $nightly->modify('Sunday')->format($date_format),
-        'beta_8'           => $nightly->modify('Tuesday')->format($date_format),
-        'beta_9'           => $nightly->modify('Thursday')->format($date_format),
-        'beta_10'          => $nightly->modify('Tuesday')->format($date_format),
-        // We have a 5 weeks beta cycle for 96,but no additional betas
-        'rc_gtb'           => $nightly->modify('Monday')->format($date_format),
-        'rc'               => $nightly->modify('Tuesday')->format($date_format),
-        'release'          => $release->format($date_format),
-    ];
-}
 // Sort the schedule by date, needed for schedules with a fixup
 asort($schedule);
 
