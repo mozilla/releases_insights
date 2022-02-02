1384b5b7cf4f6b7f28c118a0b7f165136a6c9b0f
Pascal Chevrel <pascal.chevrel@free.fr>
2022-02-02 13:54:46 +0100

    make phpstan scanning happy

--- app/models/pre4_release.php
+++ app/models/pre4_release.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use ReleaseInsights\Utils;
+use ReleaseInsights\Version;
 
 // Historical data from Product Details
 $firefox_releases = Utils::getJson('https://product-details.mozilla.org/1.0/firefox.json')['releases'];
@@ -10,6 +11,6 @@ $firefox_releases = Utils::getJson('https://product-details.mozilla.org/1.0/fire
 // Number of dot releases
 $dot_release_count = count((array) array_filter(
     $firefox_releases,
-    fn($key) => str_starts_with($key, 'firefox-' . $requested_version . '.'),
+    fn($key) => str_starts_with($key, 'firefox-' . Version::get() . '.'),
     ARRAY_FILTER_USE_KEY
 ));
