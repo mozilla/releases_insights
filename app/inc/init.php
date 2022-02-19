2ac963d0d2520b20d54f44b49cbab460c63dcb36
Pascal Chevrel <pascal.chevrel@free.fr>
2022-02-19 19:51:23 +0100

    Use filter_var on server query

--- app/inc/init.php
+++ app/inc/init.php
@@ -3,7 +3,5 @@
 declare(strict_types=1);
 
 // Dispatch urls
-$url = new ReleaseInsights\Request(
-    htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES, 'UTF-8')
-);
+$url = new ReleaseInsights\Request(filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL));
 include CONTROLLERS . $url->getController() . '.php';
