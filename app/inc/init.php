d59319b267e1ef8297f8f9f1663c0349249bc61f
Pascal Chevrel <pascal.chevrel@free.fr>
2022-02-19 19:07:45 +0100

    Refactor: Escape the use of a server provided value

--- app/inc/init.php
+++ app/inc/init.php
@@ -3,5 +3,7 @@
 declare(strict_types=1);
 
 // Dispatch urls
-$url = new ReleaseInsights\Request($_SERVER['REQUEST_URI']);
+$url = new ReleaseInsights\Request(
+    htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES, 'UTF-8')
+);
 include CONTROLLERS . $url->getController() . '.php';
