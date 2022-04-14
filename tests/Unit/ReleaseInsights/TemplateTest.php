1a87db59ee295264e945a686e7526cd11839a0c4
Pascal Chevrel <pascal.chevrel@free.fr>
2022-04-14 14:49:38 +0200

    Add a constant in tests for CI

--- tests/Unit/ReleaseInsights/TemplateTest.php
+++ tests/Unit/ReleaseInsights/TemplateTest.php
@@ -1,9 +1,10 @@
 <?php
-
 declare(strict_types=1);
 
-use ReleaseInsights\Template ;
-const INSTALL_ROOT  = __DIR__ . '/../../../';
+use ReleaseInsights\Template;
+
+const INSTALL_ROOT = __DIR__ . '/../../../';
+const CACHE_PATH = INSTALL_ROOT . '/cache/';
 
 test('Template Class', function () {
     expect((new Template('file', ['data']))->data)->toEqual(['data']);
