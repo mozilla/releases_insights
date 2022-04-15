287e9cfacc5235f27fad230aa3572fa77960de62
Pascal Chevrel <pascal.chevrel@free.fr>
2022-04-15 09:36:45 +0200

    Clean up some constant uses in unit tests

--- tests/Unit/ReleaseInsights/ConstantsTest.php
+++ tests/Unit/ReleaseInsights/ConstantsTest.php
@@ -4,10 +4,12 @@
 	We don't run any test here, but we can set up globals for all the tests
 */
 
-const CACHE_ENABLED   = false;
 const FIREFOX_RELEASE = '93';
 const FIREFOX_BETA    = '94';
 const FIREFOX_NIGHTLY = '95';
 const ESR 			  = '78';
 const UNIT_TESTING 	  = true;
-const TEST_FILES      = __DIR__ . '/../../Files/';
+const INSTALL_ROOT 	  = __DIR__ . '/../../../';
+const CACHE_PATH 	  = INSTALL_ROOT . 'cache/';
+const TEST_FILES      = INSTALL_ROOT . 'tests/Files/';
+const CACHE_ENABLED   = false;
--- tests/Unit/ReleaseInsights/TemplateTest.php
+++ tests/Unit/ReleaseInsights/TemplateTest.php
@@ -1,9 +1,7 @@
 <?php
-
 declare(strict_types=1);
 
-use ReleaseInsights\Template ;
-const INSTALL_ROOT  = __DIR__ . '/../../../';
+use ReleaseInsights\Template;
 
 test('Template Class', function () {
     expect((new Template('file', ['data']))->data)->toEqual(['data']);
