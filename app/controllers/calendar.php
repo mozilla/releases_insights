5e8843511ac0044f4f2acfe76f65079f7926fde9
Pascal Chevrel <pascal.chevrel@free.fr>
2022-07-27 13:45:14 +0200

    remove hardcoded css files + wording nits on calendar page

--- app/controllers/calendar.php
+++ app/controllers/calendar.php
@@ -7,7 +7,7 @@ $calendar = require_once MODELS . 'calendar.php';
 (new ReleaseInsights\Template(
     'calendar.html.twig',
     [
-        'page_title'        => 'Firefox Trains - General Calendar of upcoming releases',
+        'page_title'        => 'General calendar of upcoming Firefox release milestones',
         'css_page_id'       => 'calendar',
         'upcoming_releases' => (new ReleaseInsights\Data)->getFutureReleases(),
         'calendar'          => $calendar,
