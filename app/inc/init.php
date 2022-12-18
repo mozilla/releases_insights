b41ba19dc21613c71761c8ed34b146c27367ec8f
Pascal Chevrel <pascal.chevrel@free.fr>
2022-12-18 20:01:09 +0100

    Integrate Ignition error handling mibrary in dev mode

--- app/inc/init.php
+++ app/inc/init.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+include 'vendor/autoload.php';
+
+// Catch errors via Ignition library in dev mode only
+if (class_exists('\Spatie\Ignition\Ignition')) {
+    \Spatie\Ignition\Ignition::make()
+        ->setEditor('sublime')
+        ->register();
+}
+
 // Dispatch urls
 $url = new ReleaseInsights\Request(filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL));
 include CONTROLLERS . $url->getController() . '.php';
