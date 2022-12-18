d600ee21a9db957ba80d464d41d72809b80d3ccb
Pascal Chevrel <pascal.chevrel@free.fr>
2022-12-18 22:14:00 +0100

    Disable Ignition in CI as it seems to break GitHub Actions

--- app/inc/init.php
+++ app/inc/init.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 include 'vendor/autoload.php';
 
-// Catch errors via Ignition library in dev mode only
-if (class_exists('\Spatie\Ignition\Ignition')) {
-    \Spatie\Ignition\Ignition::make()
-        ->setEditor('sublime')
-        ->register();
-}
+// Load the .env file to get local vs CI CONTEXT environment value
+$dotenv = Dotenv\Dotenv::createImmutable(INSTALL_ROOT);
+$dotenv->safeLoad();
 
+// Catch errors via Ignition library in dev mode only
+if (isset($_ENV['CONTEXT']) && $_ENV['CONTEXT'] == 'local') {
+    if (class_exists('\Spatie\Ignition\Ignition')) {
+        \Spatie\Ignition\Ignition::make()
+            ->setEditor('sublime')
+            ->register();
+    }
+}
 // Dispatch urls
 $url = new ReleaseInsights\Request(filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL));
 include CONTROLLERS . $url->getController() . '.php';
