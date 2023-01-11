bcb36ed24377d6f5380ee36508db29fa5afcd932
Pascal Chevrel <pascal.chevrel@free.fr>
2023-01-11 12:11:35 +0100

    Issue #41: integrate SUMO milestones to the app

--- tests/Unit/ReleaseInsights/ReleaseTest.php
+++ tests/Unit/ReleaseInsights/ReleaseTest.php
@@ -17,6 +17,9 @@ test('Release->getSchedule()', function () {
     $obj = new Release('error');
     expect($obj->getSchedule())
         ->toBeArray();
+    $obj = new Release('110.0');
+    expect($obj->getSchedule())
+        ->toHaveKeys(['version', 'nightly_start', 'soft_code_freeze', 'string_freeze', 'merge_day', 'beta_1', 'beta_2', 'beta_3', 'sumo_1', 'beta_4', 'beta_5', 'beta_6', 'beta_7', 'sumo_2', 'beta_8', 'beta_9', 'rc_gtb', 'rc', 'release', 'planned_dot_release',]);
 });
 
 test('Release->getNiceLabel()', function () {
