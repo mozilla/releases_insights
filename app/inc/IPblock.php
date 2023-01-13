66eab18e7dff623cf8534620f1ce623e6565dfff
Pascal Chevrel <pascal.chevrel@free.fr>
2023-01-13 10:26:50 +0100

    IPBlock update

--- app/inc/IPblock.php
+++ app/inc/IPblock.php
@@ -23,7 +23,8 @@ $url_inspected = new Request(filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE
 if (Utils::inString(
     $url_inspected->request,
     ['wp-', 'adminer', 'hbk_ios', 'go.php', 'wordpress', 'phpmyadmin',
-     'xmlrpc', 'civicrm', 'backup', '_health-check', 'wallet', 'php.php', '.env', ]
+     'xmlrpc', 'civicrm', 'backup', '_health-check', 'wallet', 'php.php', '.env',
+     'vendor', 'phpunit', 'includes',]
     )) {
     if (! in_array($client_ip, $ips) ) {
         $ips[] = $client_ip;
