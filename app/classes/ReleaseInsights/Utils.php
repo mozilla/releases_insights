12faa68759a4154cb769692a33a0e6b88967c8f8
Pascal Chevrel <pascal.chevrel@free.fr>
2023-01-14 11:16:49 +0100

    Fix http code exclusion of remote ressources (typo) + add

--- app/classes/ReleaseInsights/Utils.php
+++ app/classes/ReleaseInsights/Utils.php
@@ -153,7 +153,7 @@ class Utils
         }
 
         // Remote file is not available
-        if (Utils::inString(get_headers($url)[0], ['403', '404', '500']) ) {
+        if (Utils::inString(get_headers($url)[0], ['404', '500']) ) {
             return '';// @codeCoverageIgnore
         }
 
