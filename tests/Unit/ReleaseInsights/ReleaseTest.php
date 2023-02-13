780239579aecb2b1930a0bd47b58dd775c87379b
Pascal Chevrel <pascal.chevrel@free.fr>
2023-02-13 08:51:31 +0100

    Fix timezone test to be UTC compatible and not CET

--- tests/Unit/ReleaseInsights/ReleaseTest.php
+++ tests/Unit/ReleaseInsights/ReleaseTest.php
@@ -25,7 +25,7 @@ test('Release->getSchedule()', function () {
         ->not->toHaveKeys(['beta_9']);
     $obj = new Release('112.0');
     expect($obj->getSchedule(TEST_FILES)['nightly_start'])
-        ->toBe('2023-02-13 00:00:00+01:00');
+        ->toBe('2023-02-13 00:00:00+00:00');
     $obj = new Release('116.0'); // merge day is Tuesday and we have one beta left
     expect($obj->getSchedule(TEST_FILES))
         ->toHaveKeys(['version', 'nightly_start', 'soft_code_freeze', 'string_freeze', 'merge_day', 'beta_1', 'beta_2', 'beta_3', 'sumo_1', 'beta_4', 'beta_5', 'beta_6', 'beta_7', 'sumo_2', 'beta_8', 'rc_gtb', 'rc', 'release', 'planned_dot_release',]);
