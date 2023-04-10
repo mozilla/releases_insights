6cf884551a0d3dd8fc049ee3efd1a9ea738c29f6
Pascal Chevrel <pascal.chevrel@free.fr>
2023-04-10 17:26:02 +0200

    Use a bootstrap file for phpunit, less hacky and allows launching test on a single class

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,23 +1,25 @@
 <?php
 
 /*
-	We don't run any test here, but we can set up globals for all the tests
+    We don't run any test here, but we can set up globals for all the tests
 */
 
-date_default_timezone_set('UTC');
+require_once __DIR__ . '/../vendor/autoload.php';
 
 const FIREFOX_RELEASE = '93';
 const FIREFOX_BETA    = '94';
 const FIREFOX_NIGHTLY = '95';
 const ESR             = '78';
 const UNIT_TESTING    = true;
-const INSTALL_ROOT    = __DIR__ . '/../../../';
+const INSTALL_ROOT    = __DIR__ . '/../';
 const DATA            = INSTALL_ROOT . 'app/data/';
 const CACHE_PATH      = INSTALL_ROOT . 'cache/';
 const TEST_FILES      = INSTALL_ROOT . 'tests/Files/';
 const CACHE_ENABLED   = false;
 
 // Major version numbers (integers), used across the app
-define('NIGHTLY',  (int) FIREFOX_NIGHTLY);
-define('BETA',     (int) FIREFOX_BETA);
-define('RELEASE',  (int) FIREFOX_RELEASE);
+define('NIGHTLY', (int) FIREFOX_NIGHTLY);
+define('BETA',    (int) FIREFOX_BETA);
+define('RELEASE', (int) FIREFOX_RELEASE);
+
+date_default_timezone_set('UTC');
