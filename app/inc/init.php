1bba1f4ae1afbe6788ebdfb77ccee652083eaeb2
Pascal Chevrel <pascal@mozilla.com>
2023-05-25 13:13:58 +0200

    Remove Sentry call for a test

--- app/inc/init.php
+++ app/inc/init.php
@@ -30,8 +30,3 @@ if (PRODUCTION) {
 $url = new ReleaseInsights\Request(filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL));
 
 include CONTROLLERS . $url->getController() . '.php';
-
-// Send last error to Sentry
-if (! LOCALHOST) {
-    captureLastError();
-}
