a1ef1519742ec8dd2b133b7a4a0134b708585053
Pascal Chevrel <pascal@mozilla.com>
2023-05-25 11:10:54 +0200

    Exclude dev mode for Sentry

--- app/inc/init.php
+++ app/inc/init.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use function \Sentry\{init, captureLastError};
+
 // Load the .env file to get local vs CI CONTEXT environment value
 $dotenv = Dotenv\Dotenv::createImmutable(INSTALL_ROOT);
 $dotenv->safeLoad();
@@ -14,8 +16,17 @@ if (isset($_ENV['CONTEXT']) && $_ENV['CONTEXT'] == 'local') {
             ->register();
     }
 }
+// Set up Sentry endpoint, don't send errors while in dev mode
+if (isset($_ENV['CONTEXT']) && $_ENV['CONTEXT'] !== 'local') {
+    init(['dsn' => 'https://20bef71984594e16add1d2c69146ad88@o1069899.ingest.sentry.io/4505243430092800']);
+}
 
 // Dispatch urls
 $url = new ReleaseInsights\Request(filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL));
 
 include CONTROLLERS . $url->getController() . '.php';
+
+// Send last error to Sentry
+if (isset($_ENV['CONTEXT']) && $_ENV['CONTEXT'] !== 'local') {
+    captureLastError();
+}
