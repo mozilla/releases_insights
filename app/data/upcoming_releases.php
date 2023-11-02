8c300f245b2fd70f4fbf4ffbbd09c770cc77883e
Pascal Chevrel <pascal@mozilla.com>
2023-11-02 16:39:11 +0100

    Clean up future releases array

--- app/data/upcoming_releases.php
+++ app/data/upcoming_releases.php
@@ -3,19 +3,16 @@
 declare(strict_types=1);
 
 /*
-    Dates of future releases.
+    Dates of future Firefox releases.
     We don't provide them in the product-details library.
 
-    These dates are copied from https://wiki.mozilla.org/Release_Management/Calendar
-
+    These dates are decided by Release Management
     They are needed to provide views on future releases.
+
     This is manually updated.
 */
 
 return [
-    '117.0' => '2023-08-29',
-    '118.0' => '2023-09-26',
-    '119.0' => '2023-10-24',
     '120.0' => '2023-11-21',
     '121.0' => '2023-12-19',
     '122.0' => '2024-01-23',
