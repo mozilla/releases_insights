70dcb4c5fc73c41a292b8ce6af0b07c126b9f780
Pascal Chevrel <pascal@mozilla.com>
2024-05-14 01:48:59 +0200

    Clean up relduty data

--- app/data/release_owners.php
+++ app/data/release_owners.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 /*
     Release Owners per release.
-
-    Release owners before Firefox 27 is undocumented.
-
     This is manually updated.
 */
+
 $owners = [
     'TBD'       => 'To be determined',
     'basil'     => 'Basil Hashem',
