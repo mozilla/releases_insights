7e3c429f17c2fc65660476b202ad4744d0039e49
Pascal Chevrel <pascal@mozilla.com>
2024-07-17 21:19:41 +0200

    Further calendar fixes

--- app/classes/ReleaseInsights/Release.php
+++ app/classes/ReleaseInsights/Release.php
@@ -54,7 +54,10 @@ class Release
         $schedule = [
             'nightly_start'       => $date($nightly),
             'qa_request_deadline' => $date('Friday'),
-            'qa_feature_done_1'   => $date('Friday +1 week 21:00'),
+            'qa_feature_done_1'   => match ($this->version) {
+                '142.0' => $date('July 3 21:00'),
+                default => $date('Friday +1 week 21:00'),
+            },
             'qa_feature_done_2'   => match ($this->version) {
                 '135.0'          => $date($nightly->modify('+3 weeks')->modify('Thursday 08:00')),
                 '141.0', '142.0' => $date($nightly->modify('+1 week')->modify('Wednesday 08:00')),
@@ -86,7 +89,11 @@ class Release
         ];
 
         if (! in_array( (int) $this->version, $this->no_planned_dot_releases)) {
-            $schedule += ['planned_dot_release' => $date($release->modify('+2 weeks 00:00'))];
+            if ($this->version === '146.0') {
+                $schedule += ['planned_dot_release' => $date($release->modify('December 18 00:00'))];
+            } else {
+                $schedule += ['planned_dot_release' => $date($release->modify('+2 weeks 00:00'))];
+            }
        }
 
         // Sort the schedule by date, needed for schedules with a fixup
