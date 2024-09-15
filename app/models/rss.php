b603a1876706e29982c6904331f162da5958dee0
Pascal Chevrel <pascal@mozilla.com>
2024-09-15 12:57:58 +0200

    RSS: minor code cleanup

--- app/models/rss.php
+++ app/models/rss.php
@@ -22,17 +22,16 @@ krsort($releases, SORT_NATURAL);
 // Limit our RSS feed to 30 items, roughly a year of releases
 $releases = array_slice($releases, 0, 30);
 
+$set_time = fn(string $date) => (new DateTime($date))->setTime(13, 0)->format(DateTime::RFC822);
+
 $rss = [];
 foreach ($releases as $key => $values) {
     $rss[] = [
-        'date'     => (new DateTime($values['date']))->setTime(13, 0)->format(DateTime::RFC822),
         'version'  => $key,
+        'date'     => $set_time($values['date']),
         'platform' => $values['platform'],
-        'relnotes' => 'https://www.mozilla.org/en-US/firefox/' . $key . '/releasenotes/',
     ];
 }
 
 // We take the last update to the feed as the first item (latest release) in the sorted array
-$latest_release_date = (new DateTime(reset($releases)['date']))->setTime(13,0)->format(DateTime::RFC822);
-
-return [$latest_release_date, $rss];
+return [$set_time(reset($releases)['date']), $rss];
