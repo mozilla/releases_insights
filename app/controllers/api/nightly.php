9bfdc21f0bf868c4e51f8ecef0cba577e21752d8
Pascal Chevrel <pascal@mozilla.com>
2024-09-30 16:55:24 +0200

    Refactoring

--- app/controllers/api/nightly.php
+++ app/controllers/api/nightly.php
@@ -4,15 +4,10 @@ declare(strict_types=1);
 
 use ReleaseInsights\{Json, Model};
 
-$data = (new Model('api_nightly'))->get();
-
-
-
-// We want to send a simplified Json for our public API
-$data_for_api = [];
-foreach ($data as $key => $values) {
-    $data_for_api[$key] = $values['revision'];
+// We want to send simplified Json data for our public API
+$data = [];
+foreach ((new Model('api_nightly'))->get() as $key => $values) {
+    $data[$key] = $values['revision'];
 }
 
-\ReleaseInsights\Debug::dump($data, $data_for_api);
-(new Json($data_for_api))->render();
+(new Json($data))->render();
