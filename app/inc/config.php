815b92a4010b9eee783bb1e523e77466078a8928
Pascal Chevrel <pascal@mozilla.com>
2025-03-11 18:00:28 +0100

    Debug code in prod to check realpath() value in gcp

--- app/inc/config.php
+++ app/inc/config.php
@@ -16,6 +16,8 @@ locale_set_default('en-US');
 
 // Application globals paths
 define('INSTALL_ROOT', dirname(__DIR__, 2) . '/');
+define('INSTALL_ROOT2', realpath(dirname(__DIR__, 2)) . '/');
+
 const CONTROLLERS = INSTALL_ROOT . 'app/controllers/';
 const DATA        = INSTALL_ROOT . 'app/data/';
 const MODELS      = INSTALL_ROOT . 'app/models/';
