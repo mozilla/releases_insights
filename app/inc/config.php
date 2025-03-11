dc008214e8c77c7d89d796324b1de6dc9d3f9c7f
Pascal Chevrel <pascal@mozilla.com>
2025-03-11 18:32:55 +0100

    More debugging for base path

--- app/inc/config.php
+++ app/inc/config.php
@@ -18,6 +18,7 @@ locale_set_default('en-US');
 define('INSTALL_ROOT', dirname(__DIR__, 2) . '/');
 define('INSTALL_ROOT2', realpath(dirname(__DIR__, 2)) . '/');
 define('INSTALL_ROOT3', realpath('.') . '/');
+define('INSTALL_ROOT4', realpath(__DIR__ . '/../../') . '/');
 
 const CONTROLLERS = INSTALL_ROOT . 'app/controllers/';
 const DATA        = INSTALL_ROOT . 'app/data/';
