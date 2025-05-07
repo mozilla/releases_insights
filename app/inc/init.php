87a7e963e1efc301d8c9fc6f77f3368b3fe9f24b
Pascal Chevrel <pascal@mozilla.com>
2025-05-07 14:19:47 +0200

    Restablish stricter CSP rules. Change default text editor for error handler page in dev mode

--- app/inc/init.php
+++ app/inc/init.php
@@ -5,21 +5,26 @@ declare(strict_types=1);
 use ReleaseInsights\Request;
 use function Sentry\captureLastError;
 
+// Allow http resources when ran locally in tests
+$https_only = LOCALHOST ? '' : 'default-src https:;';
+
+// This is our production CSP
+$csp_headers = "Content-Security-Policy: $https_only object-src 'none'; base-uri 'self'; script-src 'self' 'nonce-" . NONCE ."'; style-src 'self' 'nonce-" . NONCE . "'; style-src-attr 'unsafe-inline'; frame-ancestors 'none'";
+
 // Catch errors via Ignition library in dev mode only
 if (getenv('TESTING_CONTEXT') === false  && LOCALHOST) {
     if (class_exists(\Spatie\Ignition\Ignition::class)) {
         \Spatie\Ignition\Ignition::make()
-            ->setEditor('vscode')
+            ->setEditor('sublime')
             ->register();
     }
+    // Error handler page is blocked by our production CSP rules
+    $csp_headers = '';
 }
 
 // Send HTTP security headers (not set by the server)
 header('X-Content-Type-Options: nosniff');
-
-// Allow http resources when ran locally
-$https_only = LOCALHOST ? '' : 'default-src https:;';
-// header("Content-Security-Policy: $https_only object-src 'none'; base-uri 'self'; script-src 'self' 'nonce-" . NONCE ."'; style-src 'self' 'nonce-" . NONCE . "'; style-src-attr 'unsafe-inline'; frame-ancestors 'none'");
+header($csp_headers);
 
 // Dispatch urls. The $url object is defined in router.php
 $url->loadController();
@@ -27,5 +32,5 @@ $url->loadController();
 // Send the last error to Sentry
 captureLastError();
 
-// Web request stops here
+// Make sure web request stops here
 exit;
