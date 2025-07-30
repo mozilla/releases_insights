cd8fc0dad1a83b17c5a16779b3852fa43d6e45bb
Pascal Chevrel <pascal@mozilla.com>
2025-07-30 14:44:23 +0200

    148 QA milestones +1 week

--- app/classes/ReleaseInsights/Release.php
+++ app/classes/ReleaseInsights/Release.php
@@ -68,7 +68,10 @@ class Release
             'qa_request_deadline'   => $date($nightly->modify('-3 days')),
             'nightly_start'         => $date($nightly->modify('+3 days')),
             'a11y_request_deadline' => $date('Friday'),
-            'qa_feature_done'       => $date('Friday +1 week 21:00'),
+            'qa_feature_done'       => match ($this->version->normalized) {
+                '148.0' => $date('Friday +2 weeks 21:00'),
+                default => $date('Friday +1 week 21:00'),
+            },
             'qa_test_plan_due'      => $date('Friday 21:00'),
             'soft_code_freeze'      => $date($nightly->modify('+1 week')->modify('Thursday 08:00')),
             'qa_pre_merge_done'     => $date('Friday 14:00'),
