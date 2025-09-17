54cf4a6ea899c68e23ca2a04a00e1fa43b5d81e8
Donal Meehan <76789979+DonalMe@users.noreply.github.com>
2025-09-17 12:24:50 -0400

    Update the Release Notes draft document URL to the Fx144 doc

--- app/controllers/relnotes_doc.php
+++ app/controllers/relnotes_doc.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // Link to the current Release Notes draft doc. Update that link every cycle.
-$doc = 'https://docs.google.com/document/d/1i1e1nfF142UfMC_48mtswStbLKj9LHkGzF9__9KfYj4/edit?usp=sharing';
+$doc = 'https://docs.google.com/document/d/1LOMOgr3E2cfvOeznHZwIZohvb2BbFpbBf0wCyshEEWA/edit?usp=sharing';
 
 header("Location: $doc", true, 302);
 exit;
