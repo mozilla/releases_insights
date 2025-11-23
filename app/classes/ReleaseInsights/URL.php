0bac5615c49f72c783a5586d7214970772e9fc24
Pascal Chevrel <pascal@mozilla.com>
2025-11-23 13:36:34 +0100

    Pollbot moved to a new domain over the week end and no redirect was set, breaking the release page

--- app/classes/ReleaseInsights/URL.php
+++ app/classes/ReleaseInsights/URL.php
@@ -13,7 +13,7 @@ enum URL: string
     case Balrog         = 'https://aus-api.mozilla.org/api/v1/';
     case Socorro        = 'https://crash-stats.mozilla.org/api/';
     case Archive        = 'https://archive.mozilla.org/';
-    case Pollbot        = 'https://pollbot.services.mozilla.com/v1/';
+    case Pollbot        = 'https://prod.pollbot.prod.webservices.mozgcp.net/v1/';
 
     public function target(): string
     {
