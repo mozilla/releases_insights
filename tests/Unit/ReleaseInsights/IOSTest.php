2149b4f19feba90ce6919200af036175a3144ae7
DonalMe <76789979+DonalMe@users.noreply.github.com>
2025-12-11 11:03:27 -0500

    Add complete test coverage for the 147 iOS milestones

--- tests/Unit/ReleaseInsights/IOSTest.php
+++ tests/Unit/ReleaseInsights/IOSTest.php
@@ -21,7 +21,8 @@ test('IOS->getSchedule()', function () {
     expect($obj->getPastSchedule())->not()->toHaveKeys(['dot_release_2', 'dot_release_3',]);
 
     $obj = new IOS('147.0'); // From the full schedule
-    expect($obj->getSchedule())->toHaveKeys(['version', 'merge_day_0', 'rc_gtb_0', 'qa_pre_signoff_0', 'qa_signoff_0', 'appstore_sent_0', 'merge_day_1', 'rc_gtb_1', 'qa_pre_signoff_1', 'release_0', 'qa_signoff_1', 'appstore_sent_1', 'merge_day_2', 'rc_gtb_2', 'qa_pre_signoff_2', 'release_1', 'qa_signoff_2', 'appstore_sent_2', 'merge_day_3', 'rc_gtb_3', 'qa_pre_signoff_3', 'release_2', 'qa_signoff_3', 'appstore_sent_3', 'release_3', ]);
+    expect($obj->getSchedule())->toHaveKeys(['version', 'merge_day_0', 'rc_gtb_0', 'qa_pre_signoff_0', 'qa_signoff_0', 'appstore_sent_0', 'merge_day_1', 'rc_gtb_1', 'qa_pre_signoff_1', 'release_0', 'qa_signoff_1', 'appstore_sent_1', 'merge_day_2', 'rc_gtb_2', 'qa_pre_signoff_2', 'release_1', 'qa_signoff_2', 'appstore_sent_2', 'merge_day_3', 'rc_gtb_3', 'qa_pre_signoff_3', 'release_2', 'qa_signoff_3', 'appstore_sent_3', 'merge_day_4', 'rc_gtb_4', 'qa_pre_signoff_4', 'release_3', 'qa_signoff_4', 'appstore_sent_4', 'merge_day_5', 'rc_gtb_5', 'qa_pre_signoff_5', 'release_4', 'qa_signoff_5', 'appstore_sent_5', 'release_5',]);
+    expect($obj->getPastSchedule())->toHaveKeys(['dot_release_4', 'dot_release_5',]);
 
     $obj = new IOS('148.0'); // Wellness
     expect($obj->getSchedule()['merge_day_3'])->toBe('2026-03-05 00:00:00+00:00');
