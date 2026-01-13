effec2ed419130f6329872576e1b36165d76f327
Donal Meehan <76789979+DonalMe@users.noreply.github.com>
2026-01-13 07:56:45 -0500

    Update Beta release notes URL for 148

--- app/controllers/relnotes_doc.php
+++ app/controllers/relnotes_doc.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // Link to the current Release Notes draft doc. Update that link every cycle.
-$doc = 'https://docs.google.com/document/d/14sSVmKIklesqYEj_NLXpYTkaqmJttCvFWeqOksJiWgs/edit?usp=sharing';
+$doc = 'https://docs.google.com/document/d/1j5Mo8cOgZemMFv50pYq1SvnynyRCCjYVDse8SQtHKYg/edit?usp=sharing';
 
 header("Location: $doc", true, 302);
 exit;
