a627df89650d32744eb03ca094833ca8f00b4a71
DonalMe <76789979+DonalMe@users.noreply.github.com>
2026-04-21 13:28:42 -0400

    Update Beta release notes URL for 151

--- app/controllers/relnotes_doc.php
+++ app/controllers/relnotes_doc.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // Link to the current Release Notes draft doc. Update that link every cycle.
-$doc = 'https://docs.google.com/document/d/1X8FwFbP-bBHw5WVD4FU1VELXxOOGkd6L5pU2KLNOEcY/edit?usp=sharing';
+$doc = 'https://docs.google.com/document/d/1zNeBR5IwlMp2Hi5WItwY33TekGwbPcGq5vqWyeZ6h98/edit?usp=sharing';
 
 header("Location: $doc", true, 302);
 exit;
