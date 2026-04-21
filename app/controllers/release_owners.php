74bc4819b957816e3c78755ebbff154de3325251
Pascal Chevrel <pascal@mozilla.com>
2026-04-21 17:19:59 +0200

    Clean up release owners styles page to be consistent with rest of site

--- app/controllers/release_owners.php
+++ app/controllers/release_owners.php
@@ -7,7 +7,7 @@ use ReleaseInsights\{Model, Template};
 new Template(
     'release_owners.html.twig',
     [
-        'page_title'  => 'Major releases per release owner',
+        'page_title'  => 'Major releases per Release Owner',
         'css_page_id' => 'release_owners',
         'owners'      => new Model('owners')->get(),
     ]
