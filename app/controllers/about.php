0704eeb0852b4fccf6b4893baed26910f861e751
Pascal Chevrel <pascal@mozilla.com>
2026-04-22 11:28:23 +0200

    Move about page to a twig template

--- app/controllers/about.php
+++ app/controllers/about.php
@@ -7,10 +7,10 @@ use ReleaseInsights\{Model, Template};
 $data = new Model('about')->get();
 
 new Template(
-    'regular.html.twig',
+    'about.html.twig',
     [
-        'page_title'   => 'Firefox Trains resources',
-        'css_page_id'  => 'about',
-        'page_content' => $data,
+        'page_title' => 'Firefox Trains resources',
+        'css_page_id' => 'about',
+        'yesterday'  => $data['yesterday'],
     ]
 )->render();
