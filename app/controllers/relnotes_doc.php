b40f88796fc87e4f6ad464c9e5d7e86650ad583a
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2026-05-20 12:45:11 -0400

    Update Beta release notes doc link for 152

--- app/controllers/relnotes_doc.php
+++ app/controllers/relnotes_doc.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // Link to the current Release Notes draft doc. Update that link every cycle.
-$doc = 'https://docs.google.com/document/d/1zNeBR5IwlMp2Hi5WItwY33TekGwbPcGq5vqWyeZ6h98/edit?usp=sharing';
+$doc = 'https://docs.google.com/document/d/1ZQT0UWHkITXrZ9_CQ8Bwv184i_ImzMPcggP7BCTxpVo/edit?usp=sharing';
 
 header("Location: $doc", true, 302);
 exit;
