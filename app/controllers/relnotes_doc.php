43700bded4d2db0a0542470a4fc258c93327b161
dsmithpadilla <88508950+dsmithpadilla@users.noreply.github.com>
2026-07-22 13:40:32 -0400

    Update Beta release notes doc link for 154

--- app/controllers/relnotes_doc.php
+++ app/controllers/relnotes_doc.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // Link to the current Release Notes draft doc. Update that link every cycle.
-$doc = 'https://docs.google.com/document/d/1wX5O9BsPcBSw6cR-hbGBt9MwpN_Kz2AZpXiIdejIzoU/edit?usp=sharing';
+$doc = 'https://docs.google.com/document/d/1HdjGamQZZApoGyTF_qXQ5AcqzI3JsyX-Lai8-1QdhPs/edit?usp=sharing';
 
 header("Location: $doc", true, 302);
 exit;
