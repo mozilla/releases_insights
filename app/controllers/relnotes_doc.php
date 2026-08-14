f494d1de80f5c77e43a80012384f1db5d3e35db4
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2026-08-13 21:46:07 -0400

    Switch release notes draft doc link to 155

--- app/controllers/relnotes_doc.php
+++ app/controllers/relnotes_doc.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // Link to the current Release Notes draft doc. Update that link every cycle.
-$doc = 'https://docs.google.com/document/d/1HdjGamQZZApoGyTF_qXQ5AcqzI3JsyX-Lai8-1QdhPs/edit?usp=sharing';
+$doc = 'https://docs.google.com/document/d/1_Pm4yelKsjGZkj8DV3X-w8WyvAN8EcmnScEEgbaFFAU/edit?usp=sharing';
 
 header("Location: $doc", true, 302);
 exit;
