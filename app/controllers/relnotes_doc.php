a2e621bdb163d0d765a117e47da59892ad61e01e
Pascal Chevrel <pascal@mozilla.com>
2026-09-10 17:24:32 +0200

    Link to 157 relnotes doc

--- app/controllers/relnotes_doc.php
+++ app/controllers/relnotes_doc.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // Link to the current Release Notes draft doc. Update that link every cycle.
-$doc = 'https://docs.google.com/document/d/1i_dQ993vGYS___EJtTd72bWidi8TNQz_RBvNfs4ow78/edit?usp=sharing';
+$doc = 'https://docs.google.com/document/d/1tCp_68IpgMwityJjRSBet1Yi1YC81HYza7n3onb_oAc/edit?usp=sharing';
 
 header("Location: $doc", true, 302);
 exit;
