f3bd3053a019ea1574b7b0696403d6be3fcacf74
dsmithpadilla <88508950+dsmithpadilla@users.noreply.github.com>
2026-09-24 12:44:03 -0400

    Switch release notes draft doc link to 158

--- app/controllers/relnotes_doc.php
+++ app/controllers/relnotes_doc.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // Link to the current Release Notes draft doc. Update that link every cycle.
-$doc = 'https://docs.google.com/document/d/1tCp_68IpgMwityJjRSBet1Yi1YC81HYza7n3onb_oAc/edit?usp=sharing';
+$doc = 'https://docs.google.com/document/d/17aB7a2fDnHSEgcPq-1ibC0euwU_OVsjFyAT8Fk47vmI/edit?usp=sharing';
 
 header("Location: $doc", true, 302);
 exit;
